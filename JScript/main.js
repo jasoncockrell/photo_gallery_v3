d63bc294923ffcaf93f83830c88a94b1185ac42c
Jason Cockrell <jason.cockrell@gmail.com>
2018-09-19 13:14:45 -0400

    Updating Project 4

---
+++ JScript/main.js
@@ -0,0 +1,5 @@
+//Search input to display correlating pictures
+
+let myCaptions = ['Hay Bales','Lake','Canyon','Iceberg'];
+let myInput = document.getElementById('myInput')
+let filter = input.value.toUpperCase();
