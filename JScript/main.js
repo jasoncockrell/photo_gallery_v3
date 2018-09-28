1431b03d77298204eb8a3ce6c83a22a6ad6a7648
Jason Cockrell <jason.cockrell@gmail.com>
2018-09-28 18:42:09 -0400

    Updated Photo Gallery

--- JScript/main.js
+++ JScript/main.js
@@ -1,5 +1,1 @@
 //Search input to display correlating pictures
-
-let myCaptions = ['Hay Bales','Lake','Canyon','Iceberg'];
-let myInput = document.getElementById('myInput')
-let filter = input.value.toUpperCase();
