6b3d15620612245df0c6d45b7a1e9f6db6fc7e40
Jason Cockrell <jason.cockrell@gmail.com>
2018-10-22 13:02:01 -0400

    Final Update

--- JScript/main.js
+++ JScript/main.js
@@ -1,1 +1,14 @@
-//Search input to display correlating pictures
+function photoSearch() {
+    var input, filter, li, a, i;
+      input = document.getElementById('searchInput');
+      filter = input.value.toUpperCase('photoList');
+      li = document.getElementsByTagName("li");
+      for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("a")[0];
+          if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+              li[i].style.display = "";
+          } else {
+              li[i].style.display = "none";
+          }
+      }
+  }
