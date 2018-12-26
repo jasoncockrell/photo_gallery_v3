c884091989e37267b3099ac74f55425fee782fac
Jason Cockrell <jason.cockrell@gmail.com>
2018-12-25 21:54:01 -0500

    Christmas Update
    Updated with Mobile First approach. Suggested HTML corrections.

--- JScript/main.js
+++ JScript/main.js
@@ -5,7 +5,7 @@ function photoSearch() {
       li = document.getElementsByTagName("li");
       for (i = 0; i < li.length; i++) {
           a = li[i].getElementsByTagName("a")[0].getAttribute("data-title");
-          if (a.attributeName.toUpperCase().indexOf(filter) > -1) {
+          if (a.toUpperCase().indexOf(filter) > -1) {
               li[i].style.display = "";
           } else {
               li[i].style.display = "none";
